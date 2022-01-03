0094a780f946d0e8bfcfcaa5aef36a3e9404102b
Arghya Banerjee <arghya.banerjee.dev@gmail.com>
2022-01-03 12:07:17 +0530

    Update openYoutube.py

--- web/openYoutube.py
+++ web/openYoutube.py
@@ -7,3 +7,4 @@ def openGoogle():
     edge_path="C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"
     webbrowser.register('edge', None, webbrowser.BackgroundBrowser(edge_path))
     webbrowser.get('edge').open_new_tab(urL) 
+
