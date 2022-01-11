a9d5e2151e1e99973ca3be93537b9fbde99c6461
Arghya Banerjee <arghya.banerjee.dev@gmail.com>
2022-01-11 20:06:11 +0530

    Update alarm_test.py

--- test/alarm_test.py
+++ test/alarm_test.py
@@ -6,19 +6,32 @@ def alarm(desH, desM):
 
     nowH = datetime.datetime.now().hour
     nowM = datetime.datetime.now().minute
+
+    if nowH == desH and nowM == desM:
+
+        plyer.notification.notify(
+        title="Alarm You Set",
+        message="This is your alarm",
+        timeout=5
+    )
+
     
 
 
 
 if __name__ == '__main__':
 
-    
     desH = int(input("Hour of your alarm?"))
     desM = int(input("Minute of your alarm?"))
 
+    Lag = True 
 
-    plyer.facades.notification.notify(
-        title="Alarm You Set",
-        message="This is your alarm",
-        timeout=5
-    )
+    while Lag == True:
+        alarm(desH, desM)
+
+        Lag == False
+
+
+
+
+   
